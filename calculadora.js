46091e0f705f205b9b28ca876d1e362f1eb70feb
heloisamtx <lolomachadoteixeira@gmail.com>
2023-08-29 15:40:54 -0300

    calculadora completa

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,11 @@
 var somar = require("./modulos/somar");
 var media = require("./modulos/media");
+var multiplicar = require("./modulos/multiplicar");
+var dividir = require("./modulos/dividir");
+var subtrair = require("./modulos/subtrair");
 
-console.log(somar(20,90));
+console.log("A soma de X e Y é: "+somar(20,90));
 console.log(media(20,90));
+console.log(multiplicar(20,90));
+console.log(dividir(20,90));
+console.log(subtrair(20,90));
